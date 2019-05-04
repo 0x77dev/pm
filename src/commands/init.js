0cfc2ddfb050ae99864f3af0700621317aaa4a0a
Михаил Мариненко <0x77dev@protonmail.com>
2019-05-04 15:26:14 +0300

    DO NOT USE! DEV BACKUP #backup

--- src/commands/init.js
+++ src/commands/init.js
@@ -6,11 +6,18 @@ class Init extends Command {
   async run() {
     const {flags} = this.parse(Init)
     const name = flags.name;
-    this.log('pmgr init v0.0.1');
-    this.log(`Initializing project: ${chalk.bgBlack.green(name)}.`);
-    this.log(`$ mkdir ${name} \n ${chalk.bgBlack.green(exec(`mkdir ${name}`))}`);
-    this.log(`$ git init \n ${chalk.bgBlack.green(exec(`sh -c "cd ${name} && git init" `))}`);
-    this.log(`$ yarn init -y \n ${chalk.bgBlack.green(exec(`sh -c "cd ${name} && yarn init -y" `))}`);
+    let modulesAutoInstall = flags.modulesAutoInstall;
+    this.log('pmgr init v0.0.2');
+    await this.log(`Initializing project: ${chalk.bgBlack.green(name)}.`);
+    await this.log(`$ mkdir ${name} \n ${chalk.bgBlack.green(exec(`mkdir ${name}`))}`);
+    await this.log(`$ git init \n ${chalk.bgBlack.green(exec(`sh -c "cd ${name} && git init" `))}`);
+    await this.log(`$ yarn init -y \n ${chalk.bgBlack.green(exec(`sh -c "cd ${name} && yarn init -y" `))}`);
+    if (modulesAutoInstall !== undefined && modulesAutoInstall !== '') {
+      modulesAutoInstall = modulesAutoInstall.split(',');
+      modulesAutoInstall.forEach(async Module => {
+        await this.log(`$ yarn add ${Module} \n ${chalk.bgBlack.green(exec(`sh -c "cd ${name} && yarn add ${Module}" `))}`);
+      });
+    }
     this.log(`Done: ${chalk.bgBlack.green(name)}`);
   }
 }
@@ -21,6 +28,7 @@ Init with New Project with git and npm (or yarn).
 
 Init.flags = {
   name: flags.string({char: 'n', description: 'name of the project'}),
+  modulesAutoInstall: flags.string({char: 'm', description: 'Modules to install.'}),
 }
 
 module.exports = Init;
